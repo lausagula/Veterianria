fd01d9151612fa6ca47c77b2de22458fcf4b2bed
Lautaro Sagula <lautarosag@hotmail.com>
2023-05-27 23:43:24 -0300

    Se arreglo los errores de la demo al mostrar las adopciones

--- js/mostrarAdopciones.js
+++ js/mostrarAdopciones.js
@@ -5,99 +5,100 @@ var  contenedor = document.getElementById('contenedorDatos');
 fetch (url+"/php/mostrar-adopciones.php")
 .then(res => res.json())
 .then(data  => {
-    console.log (data); 
     if(data.exito){
         data.data.forEach(datos => {
-            var div = document.createElement('div');        
-        // Raza
-        var razaTitulo = document.createElement('strong');
-        razaTitulo.textContent = 'Raza: ';
-        div.appendChild(razaTitulo);
-        div.appendChild(document.createTextNode(datos.raza_perro));
+            var div = document.createElement('div');
 
-        // Sexo
-        var sexoTitulo = document.createElement('strong');
-        sexoTitulo.textContent = 'Sexo: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(sexoTitulo);
-        div.appendChild(document.createTextNode(datos.sexo));
+            console.log(data.data.length);
+            
+            if (localStorage.getItem('email') != datos.mail){
+                // Raza
+                var razaTitulo = document.createElement('strong');
+                razaTitulo.textContent = 'Raza: ';
+                div.appendChild(razaTitulo);
+                div.appendChild(document.createTextNode(datos.raza_perro));
 
-        // Edad
-        var edadTitulo = document.createElement('strong');
-        edadTitulo.textContent = 'Edad: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(edadTitulo);
-        console.log (datos.edad);
-        div.appendChild(document.createTextNode(datos.edad));
+                // Sexo
+                var sexoTitulo = document.createElement('strong');
+                sexoTitulo.textContent = 'Sexo: ';
+                div.appendChild(document.createElement('br'));
+                div.appendChild(sexoTitulo);
+                div.appendChild(document.createTextNode(datos.sexo));
 
-        // Zona
-        var zonaTitulo = document.createElement('strong');
-        zonaTitulo.textContent = 'Zona: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(zonaTitulo);
-        div.appendChild(document.createTextNode(datos.zona));
+                // Edad
+                var edadTitulo = document.createElement('strong');
+                edadTitulo.textContent = 'Edad: ';
+                div.appendChild(document.createElement('br'));
+                div.appendChild(edadTitulo);
+                console.log (datos.edad);
+                div.appendChild(document.createTextNode(datos.edad));
 
-        // Características
-        var caracteristicasTitulo = document.createElement('strong');
-        caracteristicasTitulo.textContent = 'Características: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(caracteristicasTitulo);
-        div.appendChild(document.createTextNode(datos.caracteristicas));
+                // Zona
+                var zonaTitulo = document.createElement('strong');
+                zonaTitulo.textContent = 'Zona: ';
+                div.appendChild(document.createElement('br'));
+                div.appendChild(zonaTitulo);
+                div.appendChild(document.createTextNode(datos.zona));
 
-        // Comportamiento
-        var comportamientoTitulo = document.createElement('strong');
-        comportamientoTitulo.textContent = 'Comportamiento: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(comportamientoTitulo);
-        div.appendChild(document.createTextNode(datos.comportamiento));
+                // Características
+                var caracteristicasTitulo = document.createElement('strong');
+                caracteristicasTitulo.textContent = 'Características: ';
+                div.appendChild(document.createElement('br'));
+                div.appendChild(caracteristicasTitulo);
+                div.appendChild(document.createTextNode(datos.caracteristicas));
 
-        // Email
-        var emailTitulo = document.createElement('strong');
-        emailTitulo.textContent = 'Email: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(emailTitulo);
-        div.appendChild(document.createTextNode(datos.mail));
+                // Comportamiento
+                var comportamientoTitulo = document.createElement('strong');
+                comportamientoTitulo.textContent = 'Comportamiento: ';
+                div.appendChild(document.createElement('br'));
+                div.appendChild(comportamientoTitulo);
+                div.appendChild(document.createTextNode(datos.comportamiento));
 
-        // Estado
-        var estadoTitulo = document.createElement('strong');
-        estadoTitulo.textContent = 'Estado: ';
-        div.appendChild(document.createElement('br'));
-        div.appendChild(estadoTitulo);
-        console.log ("ver");
-        console.log (datos.estado == 0);
-        if (datos.estado == 0){
-            div.appendChild(document.createTextNode("Disponible para adopción"));
-        }else{
-            div.appendChild(document.createTextNode("Adoptado"));
-        }
-        
-        div.appendChild(document.createElement('br'));
-        div.appendChild(document.createElement('br'));
+                // Estado
+                var estadoTitulo = document.createElement('strong');
+                estadoTitulo.textContent = 'Estado: ';
+                div.appendChild(document.createElement('br'));
+                div.appendChild(estadoTitulo);
+                console.log ("ver");
+                console.log (datos.estado == 0);
+                if (datos.estado == 0){
+                    div.appendChild(document.createTextNode("Disponible para adopción"));
+                }else{
+                    div.appendChild(document.createTextNode("Adoptado"));
+                }
+                
+                div.appendChild(document.createElement('br'));
+                div.appendChild(document.createElement('br'));
 
-        var button = document.createElement('button');
-        button.textContent = 'Adoptar';
-        button.setAttribute('data-email', datos.mail);
+                var button = document.createElement('button');
+                button.textContent = 'Adoptar';
+                button.setAttribute('data-email', datos.mail);
 
-        document.getElementById('contenedorDatos').appendChild(div);
-        var button = document.createElement('button');
-        button.textContent = 'Adoptar';
-        button.setAttribute('data-email', datos.mail);
-        document.getElementById('contenedorDatos').appendChild(button);
-        document.getElementById('contenedorDatos').appendChild(document.createElement('hr'));
-        document.getElementById('contenedorDatos').appendChild(document.createElement('hr'));
-        
+                document.getElementById('contenedorDatos').appendChild(div);
+                var button = document.createElement('button');
+                button.textContent = 'Adoptar';
+                button.setAttribute('data-email', datos.mail);
+                if (datos.estado == 1){
+                    button.disabled = true;
+                }
+                document.getElementById('contenedorDatos').appendChild(button);
+                document.getElementById('contenedorDatos').appendChild(document.createElement('hr'));
+                document.getElementById('contenedorDatos').appendChild(document.createElement('hr'));
+                
 
-        button.addEventListener('click', function(event) {
-            var email = event.target.dataset.email;
-            window.location.href = (url+'/formAdopcion.html');
-            console.log('Email de contacto:', email);
-          });            
+                button.addEventListener('click', function(event) {
+                    var email = event.target.dataset.email;
+                    window.location.href = (url+'/formAdopcion.html');
+                    console.log('Email de contacto:', email);
+                });
+            }else if (data.data.length = 1){
+                div.appendChild(document.createTextNode('No hay adopciones disponibles por el momento'));
+                document.getElementById('contenedorDatos').appendChild(div);
+            }
         });
     }else{
-        console.log("No hay Adopciones");
-        var p = document.createElement('p');
-        p.textContent = 'No hay adopciones disponibles por el momento';
-        contenedor.appendChild(p);
+        div.appendChild(document.createTextNode('No hay adopciones disponibles por el momento'));
+        document.getElementById('contenedorDatos').appendChild(div);
     }
     
 })
